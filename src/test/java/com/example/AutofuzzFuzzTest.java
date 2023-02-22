175cabfaa6b122f5ae7ec5adc32257c6ae870256
freedom1b2830 <freedom1b2830@gmail.com>
2023-02-22 21:59:32 +0000

    update

--- src/test/java/com/example/AutofuzzFuzzTest.java
+++ src/test/java/com/example/AutofuzzFuzzTest.java
@@ -1,39 +1,26 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+public class AutofuzzFuzzTest {
+	/*
+	 * final static CopyOnWriteArrayList<VpnConf> confs = new
+	 * CopyOnWriteArrayList<>();
+	 *
+	 *
+	 * static @BeforeAll void init() throws IOException { final var stream =
+	 * VpnGateNetFetcher.fetchFast(); stream.forEachOrdered(conf ->
+	 * confs.addIfAbsent(conf)); }
+	 * 
+	 * @FuzzTest(maxDuration = "3m") void myFuzzTest// (final FuzzedDataProvider
+	 * data) { (final String string) { // csv->conf->ovpn1->conf->ovpn2 check
+	 * ovpn1.equals(ovpn2) // final var string = data.consumeRemainingAsString();
+	 * try { final var conf1 = VpnConfUtils.parseCSV(string); final var ovpn1 =
+	 * VpnConfUtils.toOvpnConf(conf1);
+	 * 
+	 * final var conf2 = OVPNReader.read(ovpn1); final var ovpn2 =
+	 * VpnConfUtils.toOvpnConf(conf2);
+	 * 
+	 * if (!ovpn1.equals(ovpn2)) { throw new RuntimeException("no valid parse"); } }
+	 * catch (final InputException e) { // ignore validation } }
+	 */
 
-import java.io.IOException;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import com.code_intelligence.jazzer.api.FuzzedDataProvider;
-import com.code_intelligence.jazzer.junit.FuzzTest;
-import com.github.publiclibs.vpngatenet.list.VpnGateNetFetcher;
-import com.github.publiclibs.vpngatenet.list.data.VpnConf;
-import com.github.publiclibs.vpngatenet.list.exceptions.InputException;
-import com.github.publiclibs.vpngatenet.list.utils.VpnConfUtils;
-
-class AutofuzzFuzzTest {
-	final static CopyOnWriteArrayList<VpnConf> confs = new CopyOnWriteArrayList<>();
-
-	static @BeforeAll void init() throws IOException {
-		final var stream = VpnGateNetFetcher.fetchFast();
-		stream.forEachOrdered(conf -> confs.addIfAbsent(conf));
-	}
-
-	public @FuzzTest(maxDuration = "3m") void myFuzzTest(final FuzzedDataProvider data) {
-		final var string = data.consumeRemainingAsString();
-		try {
-			VpnConfUtils.parseCSV(string);
-		} catch (final InputException e) {
-			// ignore vaidation
-		}
-	}
-
-	@Test
-	void testKeys() {
-		assertTrue(!confs.isEmpty());
-	}
 }
